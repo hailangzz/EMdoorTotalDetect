7b94bbf75a9e7496433e08b12e3242ce0523c801
hailangzz <344267342@qq.com>
2025-11-04 14:38:19 +0800

    添加fastdds 视频帧获取功能 source fastdds_env/setup.bash  python3 hand_detect.py

---
+++ blazepalmProject/RKNNDevelopmentBoardProgram/fastdds_camera_frame_detect/CMakeFiles/RGBData.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RGBData.dir/RGBDataPubSubTypes.cxx.o"
+  "CMakeFiles/RGBData.dir/RGBDataPubSubTypes.cxx.o.d"
+  "CMakeFiles/RGBData.dir/RGBDataTypeObjectSupport.cxx.o"
+  "CMakeFiles/RGBData.dir/RGBDataTypeObjectSupport.cxx.o.d"
+  "libRGBData.pdb"
+  "libRGBData.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RGBData.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
