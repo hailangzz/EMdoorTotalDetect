32ad19ce0a5b7e9b430c00232a7e34041ab41e87
hailangzz <344267342@qq.com>
2025-11-07 09:03:29 +0800

    新增每一秒，发送一次检测结果状态信息

--- blazepalmProject/RKNNDevelopmentBoardProgram/RKNNlite_Frame/fastdds_camera_frame_detect/blazepalm_utils.py
+++ blazepalmProject/RKNNDevelopmentBoardProgram/RKNNlite_Frame/fastdds_camera_frame_detect/blazepalm_utils.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from scipy.special import expit
 import os
+import time
 
 num_coords = 18
 min_score_thresh = 0.75
@@ -358,24 +359,42 @@ def image_crop_ellipse(rgb_img, image_crop_config=None):
 
 
 class HandStateController:
-    def __init__(self, threshold=7):
+
+    def __init__(self, threshold=7,time_thresh=1):
         self.threshold = threshold  # 连续帧阈值
         self.identification_number = 4
         self.counter = 0            # 当前累计帧数
-        self.state = False          # 当前状态输出
+        self.event_state = False          # 当前状态输出
+
+        self.event_time_limit = time_thresh #检测到手部的时间间隔
+        self.event_infer_send_time = time.time() #上次发送检测到手部的
+        self.event_info_send_trigger = False  # 发送手部检测时间状态标志
 
     def update(self, detected: bool):
         """
         detected: bool, 当前帧是否检测到手
         返回: bool, 当前平滑后的状态
         """
+
         if detected:
             self.counter = min(self.counter + 1, self.threshold)  # 限制最大不超过 threshold
         else:
             self.counter = max(self.counter - 1, 0)  # 限制最小不低于 0
 
         if self.counter >= self.identification_number:
-            self.state = True
+            self.event_state = True
         else:
-            self.state = False
+            self.event_state = False
 
+        if self.event_state:
+            current_time = time.time()
+            delta_t = current_time-self.event_infer_send_time
+            # print(delta_t)
+            if delta_t > self.event_time_limit:
+
+                self.event_info_send_trigger = True
+                self.event_infer_send_time = current_time
+            else:
+                self.event_info_send_trigger = False
+        else:
+            self.event_info_send_trigger = False
