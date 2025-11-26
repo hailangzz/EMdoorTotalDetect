5f99b560107b7e0c83e8b9b6d1e2e9ddb00c0227
hailangzz <344267342@qq.com>
2025-11-26 16:29:53 +0800

    新增 ros topic 图像信号传输

---
+++ AI_Toolkit_C++_program/Ros_topics/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chenkejing/catkin_ws/src
+# Build directory: /home/chenkejing/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("image_talker")
---
+++ AI_Toolkit_C++_program/Ros_topics/catkin_ws/build/image_talker/CMakeFiles/image_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/chenkejing/catkin_ws/devel/lib/image_talker/image_listener"
+  "/home/chenkejing/catkin_ws/devel/lib/image_talker/image_listener.pdb"
+  "CMakeFiles/image_listener.dir/src/image_listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
